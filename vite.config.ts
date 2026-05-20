23dcdfb85a17cd6c317d768d093000af330532e2
Vito Royeca <vito.royeca@gmail.com>
2026-05-20 17:30:52 -0400

    Fixed the RefreshRuntime has already been declared error

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
-    react(),
+    // react(),
     // babel({
     //   filter: /\.[jt]sx?$/,
     //   babelConfig: {
