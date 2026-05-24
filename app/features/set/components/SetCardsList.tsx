44b18ad20467ae5919d9f967a1d149332329e9e6
Vito Royeca <vito.royeca@gmail.com>
2026-05-23 23:37:23 -0400

    Fixed url of cards with faces

--- app/features/set/components/SetCardsList.tsx
+++ app/features/set/components/SetCardsList.tsx
@@ -29,8 +29,10 @@ const SetCardsList = ({ cards }: SetCardsListProps) => {
 
   const localImageURL = (c: MGCard) => {
     const array = c.id.split('_');
+    const hasFaces = c.faces && c.faces.length > 0;
+
     const imageURL = array.length == 3
-      ? `/images/cards/${array[0]}/${array[1]}/${array[2]}/normal.jpg`
+      ? `/images/cards/${array[0]}/${array[1]}/${array[2]}${hasFaces ? '_0' : ''}/normal.jpg`
       : c.normalURL;
 
     return imageURL;
